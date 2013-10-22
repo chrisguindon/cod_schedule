634a8d1db2dfbe5e71b27554a1979863f53e83bb
Christopher Guindon <chris.guindon@eclipse.org>
2013-10-22 09:08:59 -0400

    update file headers.

--- cod-schedule-grid.tpl.php
+++ cod-schedule-grid.tpl.php
@@ -16,7 +16,6 @@
  *   $rows are keyed by row number, fields within rows are keyed by field ID.
  * - $field_classes: An array of classes to apply to each field, indexed by
  *   field id, then row number. This matches the index in $rows.
- * @ingroup views_templates
  */
 ?>
 <table <?php if ($classes) { print 'class="'. $classes . '" '; } ?><?php print $attributes; ?>>
